76ad07adf53dbc2fdcce5c70625c9e4f6c8ba22a
CorbinLutsch <corbincooling@aol.com>
2019-01-17 14:12:56 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mandc_Assign1
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Item
+    {
+        public enum ItemType
+        {
+            Helmet, Neck, Shoulders, Back, Chest,
+            Wrist, Gloves, Belt, Pants, Boots,
+            Ring, Trinket
+        };
+
+        public enum Race { Orc, Troll, Tauren, Forsaken };
+
+        private static uint MAX_ILLVL = 360;
+        private static uint MAX_PRIMARY = 200;
+        private static uint MAX_STAMINA = 275;
+        private static uint MAX_LEVEL = 60;
+        private static uint GEAR_SLOTS = 14;
+        private static uint MAX_INVENTORY_SIZE = 20;
+
+        private readonly uint id;
+        private string name;
+        private ItemType type;
+        private uint ilvl;
+        private uint primary;
+        private uint stamina;
+        private uint requirement;
+        private string flavor;
+
+        public uint Id //this is my public property
+        {
+            get { return id;  }
+        }
+
+        public string Name //this is my public property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ItemType Type //this is my public property
+        {
+            get { return type; }
+
+            set
+            {
+                if (value >= 0 && value <= 12)
+                    type = value;
+            }
+        }
+
+        //default constructor
+        public Item()
+        {
+            id = 0;
+            name = "";
+            type = 0;
+            ilvl = 0;
+            primary = 0;
+            stamina = 0;
+            requirement = 0;
+            flavor = "";
+        }
+
+        //default constructor #2
+        public Item(uint i, string n, ItemType ty, uint ilv, uint prim, uint stam,
+        uint req, string flav)
+        {
+            id = i;
+            name = n;
+            type = ty;
+            ilvl = ilv;
+            primary = prim;
+            stamina = stam;
+            requirement = req;
+            flavor = flav;
+        }
+    }
+}
